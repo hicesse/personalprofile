4f8820c62b585d5ddfa1a2060ca0593727e9cf70
hicesse <fhutka3180@gmail.com>
2025-09-11 21:34:41 +0700

    rebuilding, adding more clear content, and a little dom

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+let nama = prompt('Siapa namamu? ');
+let intro = document.getElementById('intro');
+let kanan_2 = document.getElementById('kanan-2');
+
+while (nama === null || nama.trim() === '') {
+    nama = prompt('Siapa namamu? (tolong isi ya, jangan kosong)');
+}
+
+intro.innerText = `Hello!! ${nama} it's Haruu here!`;
+
+kanan_2.innerHTML = `I'm sorry ${nama} this website got re-building and still optimalization like the android preview, nav function (in android and desktop) and more, i'll add it soon, so please stay tuned and thank you for your understanding`;
+
+
+
