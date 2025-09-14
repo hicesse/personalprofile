66595cbaacabad6427f0e3653267b8e0297d770b
hicesse <fhutka3180@gmail.com>
2025-09-14 14:02:00 +0700

    adding some ideas on galery pge

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,13 @@
 let nama = prompt('Siapa namamu? ');
 let intro = document.getElementById('intro');
 let kanan_2 = document.getElementById('kanan-2');
+let body = document.getElementById('body');
 
 while (nama === null || nama.trim() === '') {
     nama = prompt('Siapa namamu? (tolong isi ya, jangan kosong)');
 }
 
-intro.innerText = `Hello!! ${nama} it's Haruu here!`;
+intro.innerText = `Hello!! ${nama} it's Hiban here!`;
 
 kanan_2.innerHTML = `I'm sorry ${nama} this website got re-building and still optimalization like the android preview, nav function (in android and desktop) and more, i'll add it soon, so please stay tuned and thank you for your understanding`;
 
